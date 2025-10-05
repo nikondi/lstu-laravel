19d3b86309b052589883c0ced5748d1c7d04a650
nikondi <nikondi@narod.ru>
2025-10-05 15:24:10 +0300

    Docker reconfig

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,17 @@ import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+  server: {
+    host: '0.0.0.0', // важно для Docker
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      host: 'lstu.local',
+      protocol: 'wss', // secure WebSocket
+      clientPort: 443,
+      path: "@vite/ws"
+    },
+  },
   plugins: [
     laravel({
       input: [
